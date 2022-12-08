483fc0cf0eec8b23c808dee1481beba628db95f2
hay <hayhyang@gmail.com>
2022-12-08 22:40:10 +0900

    chore: Create store

---
+++ src/lib/store.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const defaultTasks = [
+  { id: "1", title: "Something", state: "TASK_INBOX" },
+  { id: "2", title: "Something more", state: "TASK_INBOX" },
+  { id: "3", title: "Something else", state: "TASK_INBOX" },
+  { id: "4", title: "Something again", state: "TASK_INBOX" },
+];
+
+const taskBoxData = { tasks: defaultTasks, status: "idle", error: null };
+
+const taskSlice = createSlice({ name: "taskBox", initialState: taskBoxData });
